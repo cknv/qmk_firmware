6713b44bf581e47bfaab849e4c0cbfa28b7132d3
Esben Sonne <esbensonne@gmail.com>
2021-02-22 20:52:40 +0100

    Fill out QWERTY whitefox layer

--- keyboards/whitefox/keymaps/cknv/keymap.c
+++ keyboards/whitefox/keymaps/cknv/keymap.c
@@ -66,11 +66,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     // Switch to QWERTY mode for games where remapping keys is not allowed.
     [3] = LAYOUT(
-        KC_ESC, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
-        _______,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSLS,          DF(0),\
-        _______,   KC_A,   KC_S,   KC_D,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,\
-        _______,_______,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,        _______,_______,\
-        _______,_______,_______,                  KC_SPC,           _______,  _______, _______,                 _______,_______,_______ \
+        KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL,_______,KC_BSPC,_______,\
+        KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSLS,          DF(0),\
+        KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,_______, KC_ENT,        _______,\
+        KC_LSFT,_______,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,          KC_UP,_______,\
+        KC_LCTL,KC_LGUI,KC_LALT,                  KC_SPC,           KC_RGUI,  KC_RCTL, _______,                 KC_LEFT,KC_DOWN,KC_RGHT \
     ),
 };
 
