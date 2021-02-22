cf0c6af8ff2fbac38366e262b432191799c02157
Esben Sonne <esbensonne@gmail.com>
2021-02-22 20:53:04 +0100

    Improve key code alignment

--- keyboards/whitefox/keymaps/cknv/keymap.c
+++ keyboards/whitefox/keymaps/cknv/keymap.c
@@ -44,7 +44,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         TD(GRESC), KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,_______,KC_BSPC,_______,\
         KC_TAB,    KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_LBRC,KC_RBRC,KC_BSLS,          DF(3),\
-        KC_BSPC,   KC_A,   KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,_______,KC_ENT,         _______,\
+        KC_BSPC,   KC_A,   KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,_______, KC_ENT,        _______,\
         KC_LSFT,   _______,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,          KC_UP,_______,\
         KC_LCTL,   KC_LGUI,KC_LALT,            LT(1, KC_SPC),           KC_RGUI,  KC_RCTL, _______,                KC_LEFT,KC_DOWN,KC_RGHT \
     ),
