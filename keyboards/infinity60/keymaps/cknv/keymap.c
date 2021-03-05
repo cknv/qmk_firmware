0c48b71740759260d2c8ff33d0b43315103ad62e
Esben Sonne <esbensonne@gmail.com>
2021-03-05 16:53:04 +0100

    Use userspace code in infinity60 keymap

--- keyboards/infinity60/keymaps/cknv/keymap.c
+++ keyboards/infinity60/keymaps/cknv/keymap.c
@@ -1,43 +1,5 @@
 #include QMK_KEYBOARD_H
-
-#define TAPPING_TERM 100
-#define TAP_CODE_DELAY 10
-
-enum {
-    GRESC,
-};
-
-enum custom_keycodes {
-    GNAV = SAFE_RANGE,
-};
-bool is_gross_nav_active = false;
-uint16_t gross_nav_kc = 0;
-uint16_t gross_nav_timer = 0;
-static const uint8_t GROSS_NAV_MULTIPLIER = 8;
-
-// Custom grave/esc tap logic.
-// Press once for one grace, twice for esc, and three times for three graves.
-void grave_esc_tap(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            tap_code(KC_GRV);
-            break;
-        case 2:
-            tap_code(KC_ESC);
-            break;
-        case 3:
-            tap_code(KC_GRV);
-            tap_code(KC_GRV);
-            tap_code(KC_GRV);
-            break;
-    }
-    reset_tap_dance(state);
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [GRESC] = ACTION_TAP_DANCE_FN(grave_esc_tap),
-};
-
+#include "cknv.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Primary Layer
@@ -82,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Layer 2: Coarse Navigation
      * ,-----------------------------------------------------------.
-     * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+     * |   |   |   |   |   |   |   |   |   |   |   |   |   |RST|   |
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   |   |PUp|   |   |   |   |     |
      * |-----------------------------------------------------------|
@@ -94,59 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [2] = LAYOUT_60_ansi_split_bs_rshift(
-      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   \
+      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, RESET, _______,   \
       _______,_______,_______,_______,_______,_______,_______,_______,KC_PGUP  ,_______,_______,_______,_______,_______,      \
       _______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_END,_______,_______,_______,            \
       _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,            \
       _______,_______,_______,          _______,               _______,_______,_______,_______),
 };
-
-// Repeats a keycode a bunch of times.
-// This enables a style of navigation that between up/down and page up/down.
-void gross_nav(uint16_t kc) {
-    for (int i = 0; i < GROSS_NAV_MULTIPLIER; i++) {
-        tap_code(kc);
-    }
-}
-
-// Starts a gross navigation loop if GNAV and the event is pressed.
-// Otherwise resets gross_nav_kc, which stops the loop.
-// The loop consists of registering some data that the matrix_scan_user picks up on.
-bool register_gross_nav(keyrecord_t *record, uint16_t kc) {
-    if (is_gross_nav_active && record->event.pressed) {
-        gross_nav_kc = kc;
-        gross_nav_timer = timer_read();
-        gross_nav(gross_nav_kc);
-        return false;
-    }
-    gross_nav_kc = 0;
-    return true;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case GNAV:
-            // Set gross navigations as active
-            is_gross_nav_active = record->event.pressed;
-            if (!record->event.pressed) {
-                gross_nav_kc = 0;
-            }
-            return true;
-            break;
-        case KC_UP:
-        case KC_DOWN:
-        case KC_LEFT:
-        case KC_RGHT:
-            return register_gross_nav(record, keycode);
-            break;
-    }
-
-    return true;
-}
-
-void matrix_scan_user(void) {
-    if (gross_nav_kc != 0 && timer_elapsed(gross_nav_timer) > 150) {
-            gross_nav(gross_nav_kc);
-            gross_nav_timer = timer_read();
-    }
-}
