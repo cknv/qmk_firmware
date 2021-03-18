27fc5bcf09bd8803179cfcb4d40a5ddb5a6a4e3f
Esben Sonne <esbensonne@gmail.com>
2021-03-18 21:56:48 +0100

    Move backspaces around
    Switching backspace to del on L1 turns out to be annoying.
    Del is now placed on space backspace on the left.

--- keyboards/whitefox/keymaps/cknv/keymap.c
+++ keyboards/whitefox/keymaps/cknv/keymap.c
@@ -6,13 +6,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         TD(GRESC), KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,_______,KC_BSPC,_______,\
         KC_TAB,    KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_LBRC,KC_RBRC,KC_BSLS,          DF(3),\
-        KC_BSPC,   KC_A,   KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,_______, KC_ENT,        _______,\
+        KC_DEL ,   KC_A,   KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,_______, KC_ENT,        _______,\
         KC_LSFT,   _______,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,          KC_UP,_______,\
         KC_LCTL,   KC_LGUI,KC_LALT,            LT(1, KC_SPC),           KC_RGUI,  KC_RCTL, _______,                KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     // Space activated layer, navigation and misc.
     [1] = LAYOUT(
-        _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,KC_DEL ,_______,\
+        _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,_______,_______,\
         _______,KC_MSTP,KC_MPLY,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  KC_UP,XXXXXXX,XXXXXXX,KC_VOLD,KC_VOLU,XXXXXXX,        _______,\
         _______,KC_LSFT,KC_LCTL,  MO(2),   GNAV,XXXXXXX,XXXXXXX,KC_LEFT,KC_DOWN,KC_RGHT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        _______,\
         _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        _______,_______,\
